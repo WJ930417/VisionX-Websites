51a71f6a80820abd1f7ee9f215b4b338035dba50
Jing Wang <wangjing9304173123@gmail.com>
2019-12-02 15:30:32 -0800

    initial commit

--- js/navbarScroll.js
+++ js/navbarScroll.js
@@ -1,18 +1,6 @@
-/*
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if ($(window).scrollTop() < 100) {
-      $('.navbar').css({ 'background-color': 'transparent' });
-    } else {
-      $('.navbar').css({ 'background-color': 'black' });
-    }
-  });
-});
-*/
-
 $(function() {
   $(document).scroll(function() {
-    var $nav = $('.navbar-fixed-top');
+    var $nav = $('.navbar-change');
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
